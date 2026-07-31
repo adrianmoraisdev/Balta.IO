ea453c53b68e4aed2d9b5a6ab637ac4c902d8388
Admin Brisa <admin@brisaconsorcios.com.br>
2026-07-31 11:43:16 -0300

    ClassesObjetos

---
+++ Classes&Objetos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassesObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var custumer = new Custumer();
+            custumer.Name = "John";
+            custumer.LastName = "Doe";
+            custumer.Age = 30;
+            Console.WriteLine($"Name: {custumer.Name}, Last Name: {custumer.LastName}, Age: {custumer.Age}");
+        }
+    }
+    class Custumer
+    {
+        public string Name;
+        public string LastName;
+        public int Age;
+    }
+}
