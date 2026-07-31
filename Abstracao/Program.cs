c4f5a62652cdeb911bd1f0f8753d307067a05e01
Admin Brisa <admin@brisaconsorcios.com.br>
2026-07-31 11:59:22 -0300

    Abstraçao

---
+++ Abstracao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+
+        }
+
+        class Pagamento
+        {
+            //Propriedades
+            DateTime Vencimento;
+
+            //Métodos
+            void Pagar()
+            {
+                ConsultarSaldoDoCartao();
+            }
+
+            private void ConsultarSaldoDoCartao()
+            {
+
+
+            }
+
+        }
+    }
+}
