45a1d090454c3615a6ccbc82870e7f89b63d4431
Admin Brisa <admin@brisaconsorcios.com.br>
2026-07-31 14:30:39 -0300

    TiposComplexos

---
+++ TiposComplexos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TiposComplexos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        //Tipos complexos: são tipos que podem armazenar múltiplos valores e podem ter métodos e propriedades.
+        //Ex: classes, structs, arrays, listas, dicionários, etc.
+
+        public class Pagamento
+        {
+            //Propriedades
+            DateTime Vencimento;
+
+            //Relacionamento de composição: a classe Pagamento possui um objeto do tipo Address,
+            //que representa o endereço de cobrança do pagamento.
+            Address BillingAddress;
+
+            //Métodos
+            void Pagar()
+            {
+
+            }
+        }
+
+        public class Address() 
+        {
+            string ZipCode;
+        }
+
+    }
+}
