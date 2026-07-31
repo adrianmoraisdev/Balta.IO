fd0b2a26fd476a3ffc71b5b42e636297b2df6b56
Admin Brisa <admin@brisaconsorcios.com.br>
2026-07-31 13:33:13 -0300

    Heranca

---
+++ Heranca/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamentoBoleto = new PagamentoBoleto();
+            pagamentoBoleto.Pagar();
+            pagamentoBoleto.Vencimento = DateTime.Now;
+
+        }
+
+        class Pagamento
+        {
+            //Propriedades
+            public DateTime Vencimento;
+
+            //Métodos
+            public void Pagar()
+            {
+
+            }
+        }
+        class PagamentoBoleto : Pagamento
+        {
+            //Propriedades
+            string CodigoBarras;
+
+            //Métodos
+            public void Pagar()
+            {
+
+            }
+        }
+    }
+}
