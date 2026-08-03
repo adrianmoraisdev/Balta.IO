ec6d97d535d407ed56909a3f5b8bfc053d323092
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-03 10:17:11 -0300

    Propriedades

---
+++ Propriedades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+            pagamento.NumeroBoleto = "123456789";
+
+
+        }
+
+
+        public class Pagamento
+        {
+
+
+            //Propriedades
+            public string NumeroBoleto { get; set; }
+            public DateTime Vencimento { get; set; }
+
+            private DateTime _dataPagamento;
+
+            public DateTime DataPagamento
+            {
+                get {
+                    Console.WriteLine("Lendo o Valor");
+                    return _dataPagamento; }
+                set {
+                    Console.WriteLine("Atribuindo o Valor");
+                    _dataPagamento = value; }
+            }
+        }
+    }
+}
