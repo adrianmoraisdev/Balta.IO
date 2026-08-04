c333500ea383b94951d85f28cdb6f20d99da60b4
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-04 09:40:34 -0300

    PartialClass

---
+++ PartialClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var payment = new Payments.Payment();
+            payment.PropriedadeA = 10;
+            payment.PropriedadeB = 20;
+        }
+    }
+}
