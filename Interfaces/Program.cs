c1b26d091bb62cfe5678a304524b032dba6c58f1
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-04 09:50:22 -0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+            pagamento.Vencimento = DateTime.Now.AddDays(30);
+            
+        }
+    }
+
+    public class Pagamento : IPagamento
+    {
+        public DateTime Vencimento { get; set; }
+
+        public void Pagar(double valor)
+        {
+         
+        }
+    }
+
+    public class PagamentoCartao : IPagamento
+    {
+        public DateTime Vencimento { get; set; }
+        public void Pagar(double valor)
+        {
+
+        }
+    }
+
+    public interface IPagamento
+    {
+        DateTime Vencimento { get; set; }
+        void Pagar(double valor);
+    }
+}
