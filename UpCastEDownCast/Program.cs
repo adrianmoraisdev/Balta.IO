e4a6ed096266049c2cd0dba590951a10fd91bd99
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-04 10:08:53 -0300

    UpCastEDownCast

---
+++ UpCastEDownCast/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UpCastEDownCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person = new Personal();
+            person = new Corporate();
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; } 
+    }
+
+    public class Personal : Person
+    {
+        public string CPF { get; set; }
+    }
+    
+    public class Corporate : Person
+    {
+        public string CNPJ { get; set; }
+    }
+}
