6a82effe93a48de60ef497aaffd34af75e6469f2
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-04 15:02:15 -0300

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            var context = new DataContext<Person>();
+            context.Save(person);
+        }
+    }
+
+    public class DataContext<T> 
+    {
+        public void Save(T entity)
+        {
+
+        }
+    }
+
+    public class Person { }
+    public class Payament { }
+    public class Subscription { }
+}
