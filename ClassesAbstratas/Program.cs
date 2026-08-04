9391efd0bb124fff14602e550a694452a29b4320
Admin Brisa <admin@brisaconsorcios.com.br>
2026-08-04 09:58:56 -0300

    ClassesAbstratas

---
+++ ClassesAbstratas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassesAbstratas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+
+        }
+    }
+
+    // Absttract serve para definir um contrato, mas não pode ser instanciada.
+    public abstract class Pagamento : IPagamento
+    {
+        public DateTime Vencimento { get; set; }
+
+        public virtual void Pagar(double valor)
+        {
+
+        }
+    }
+
+    public class PagamentoCartao : Pagamento
+    {
+        public override void Pagar(double valor)
+        {
+           base.Pagar(valor);
+        }
+    }
+
+    public class PagamentoBoleto : Pagamento
+    {
+        public override void Pagar(double valor)
+        {
+            base.Pagar(valor);
+        }
+    }
+
+    public class PagamentoApplePay : Pagamento
+    {
+        public override void Pagar(double valor)
+        {
+            base.Pagar(valor);
+        }
+    }
+
+    public interface IPagamento
+    {
+        DateTime Vencimento { get; set; }
+        void Pagar(double valor);
+    }
+}
